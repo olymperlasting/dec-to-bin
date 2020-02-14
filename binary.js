867ad575919994ac4daa06433b4364c0b913b6c0
olymperlasting <kadelmoro@up.edu.ph>
2020-02-14 10:50:15 +0800

    Update binary.js
    Added compatibility with negative integers

--- binary.js
+++ binary.js
@@ -1,16 +1,19 @@
 function binary() {
   var x = parseInt (document.getElementById("x").value);
-  var y = x;
+  var y = Math.abs (x);
   var arr = [];
 
   while (y != 0) {
     arr.push (y % 2);
-    y = Math.trunc (y / 2);
+    y = parseInt (y / 2);
   }
 
   if (arr.length == 0) document.getElementById("result").innerHTML = "Decimal number: " + x + "<br>Binary number: 0";
-  else {
+  else if (x > 0) {
     document.getElementById("result").innerHTML = "Decimal number: " + x + "<br>Binary number: ";
+    while (arr.length != 0) document.getElementById("result").innerHTML += arr.pop();
+  } else if (x < 0) {
+    document.getElementById("result").innerHTML = "Decimal number: " + x + "<br>Binary number: -";
     while (arr.length != 0) document.getElementById("result").innerHTML += arr.pop();
   }
 }
